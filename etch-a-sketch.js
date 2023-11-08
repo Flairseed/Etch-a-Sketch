3cb808a72825f69fd0ac72d0fdaead74b0c8cced
jay <rameshpvb@gmail.com>
2023-11-09 03:27:56 +0800

    feat: enclose canvas generation in func to allow variable number of pxls

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,9 +1,16 @@
 const canvas = document.querySelector(".canvas");
-let pixels = 16
+const defaultPixels = 16;
+generatePixels(defaultPixels);
 
-for (let i = 0; i < pixels**2; i++) {
-  const pixel = document.createElement("div");
-  pixel.setAttribute("class", "canvas-pixel");
-  pixel.addEventListener("mouseenter", e => pixel.style.backgroundColor = "black");
-  canvas.appendChild(pixel);
+function generatePixels (pixelSize) {
+  while (canvas.firstChild) {
+    canvas.removeChild(canvas.lastChild);
+  }
+  canvas.style.setProperty("--pixels", pixelSize)
+  for (let i = 0; i < pixelSize**2; i++) {
+    const pixel = document.createElement("div");
+    pixel.setAttribute("class", "canvas-pixel");
+    pixel.addEventListener("mouseenter", e => pixel.style.backgroundColor = "black");
+    canvas.appendChild(pixel);
+  }
 }
