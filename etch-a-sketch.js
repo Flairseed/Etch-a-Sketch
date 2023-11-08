793b20a7cd9101a0648bbba083eb09b2642d6427
jay <rameshpvb@gmail.com>
2023-11-09 02:52:13 +0800

    feat: size the canvas properly to 500px by 500px

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,8 +1,9 @@
 const canvas = document.querySelector(".canvas");
 
-for (let i = 0; i <= 16; i++) {
+for (let i = 0; i < 16; i++) {
   const column = document.createElement("div");
-  for (let i = 0; i <= 16; i++) {
+  column.setAttribute("class", "column")
+  for (let i = 0; i < 16; i++) {
     const pixel = document.createElement("div");
     pixel.setAttribute("class", "canvas-pixel")
     pixel.addEventListener("mouseenter", e => pixel.style.backgroundColor = "black");
