c7cc0be588ba13ae5e98ef3093fee7cea99bb1e5
jay <rameshpvb@gmail.com>
2023-11-09 04:17:50 +0800

    feat: add pixel class so that rgb values can be variables

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -12,20 +12,26 @@ function generatePixels (pixelSize) {
   canvas.style.setProperty("--pixels", pixelSize)
   for (let i = 0; i < pixelSize**2; i++) {
     const pixel = document.createElement("div");
+    pixel.setAttribute("class", "pixel");
     pixel.addEventListener("mouseenter", changePixelColor);
     canvas.appendChild(pixel);
   }
 
   function changePixelColor (event) {
     const currentPixel = event.currentTarget;
-    let color;
     if (colorMode === "normal") {
-      color = "black"
+      currentPixel.style.setProperty("--red", 0)
+      currentPixel.style.setProperty("--green", 0)
+      currentPixel.style.setProperty("--blue", 0)
     }
     else if (colorMode === "rgb") {
-      color = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`
+      currentPixel.style.setProperty("--red", Math.floor(Math.random() * 255))
+      currentPixel.style.setProperty("--green", Math.floor(Math.random() * 255))
+      currentPixel.style.setProperty("--blue", Math.floor(Math.random() * 255))
     }
-    currentPixel.style.backgroundColor = color;
+    else if (colorMode === "darken") {
+      
+    }
   }
 }
 
