f1c7687795989a6f7ffec9a15b7e077df414533c
jay <rameshpvb@gmail.com>
2023-11-09 03:42:43 +0800

    feat: remove class, border for pixels add border to canvas

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -11,7 +11,6 @@ function generatePixels (pixelSize) {
   canvas.style.setProperty("--pixels", pixelSize)
   for (let i = 0; i < pixelSize**2; i++) {
     const pixel = document.createElement("div");
-    pixel.setAttribute("class", "canvas-pixel");
     pixel.addEventListener("mouseenter", e => pixel.style.backgroundColor = "black");
     canvas.appendChild(pixel);
   }
