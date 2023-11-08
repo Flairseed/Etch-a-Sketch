7adbc6d806154153d7fffd5e0cf5f12f855b1481
jay <rameshpvb@gmail.com>
2023-11-09 03:17:36 +0800

    refator: change the display to work as a grid instead of flexbox

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,13 +1,9 @@
 const canvas = document.querySelector(".canvas");
+let pixels = 16
 
-for (let i = 0; i < 16; i++) {
-  const column = document.createElement("div");
-  column.setAttribute("class", "column")
-  for (let i = 0; i < 16; i++) {
-    const pixel = document.createElement("div");
-    pixel.setAttribute("class", "canvas-pixel")
-    pixel.addEventListener("mouseenter", e => pixel.style.backgroundColor = "black");
-    column.appendChild(pixel)
-  }
-  canvas.appendChild(column);
+for (let i = 0; i < pixels**2; i++) {
+  const pixel = document.createElement("div");
+  pixel.setAttribute("class", "canvas-pixel");
+  pixel.addEventListener("mouseenter", e => pixel.style.backgroundColor = "black");
+  canvas.appendChild(pixel);
 }
