3988974ef09f21f30119b4b57bcfa12d2337f66b
jay <rameshpvb@gmail.com>
2023-11-08 21:53:36 +0800

    refactor: change row to column

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,12 +1,12 @@
 const canvas = document.querySelector(".canvas");
 
 for (let i = 0; i <= 16; i++) {
-  const row = document.createElement("div");
+  const column = document.createElement("div");
   for (let i = 0; i <= 16; i++) {
     const pixel = document.createElement("div");
     pixel.setAttribute("class", "canvas-pixel")
     pixel.addEventListener("mouseenter", e => pixel.style.backgroundColor = "black");
-    row.appendChild(pixel)
+    column.appendChild(pixel)
   }
-  canvas.appendChild(row);
+  canvas.appendChild(column);
 }
